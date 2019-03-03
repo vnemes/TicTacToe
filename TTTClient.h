32b927eea4c958d5deb02d5bd54a0cb53d9ba945
vnemes <vladnemes5@gmail.com>
2019-03-03 10:35:31 -0500

    Updates client to method outside main

--- TTTClient.h
+++ TTTClient.h
@@ -5,4 +5,17 @@
 #ifndef TICTACTOE_TTTCLIENT_H
 #define TICTACTOE_TTTCLIENT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+//http://www.linuxhowtos.org/C_C++/socket.htm
+
+int client_connect(int portno, char *server_host);
+
 #endif //TICTACTOE_TTTCLIENT_H
--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
 #include <stdio.h>
+#include "TTTServer.h"
+#include "TTTClient.h"
 
 
-
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
+    printf("Please select run mode: \n"); 
+    printf("1. Server\n");
+    printf("2. Client\n");
+    int choice;
+    scanf("%d", &choice);
+    if (choice == 1)
+        server_listen(2020);
+    else
+        client_connect(2020, "localhost");
     return 0;
 }
